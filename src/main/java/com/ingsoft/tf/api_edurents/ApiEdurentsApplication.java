8bbe64dd1984357f3a83cebe17d7a66fa71f3fc8
EduRS14 <eduardo14.05rivas@gmail.com>
2025-05-03 00:29:30 -0500

    feature: Agregado de archivos del proyecto Spring Boot base

---
+++ src/main/java/com/ingsoft/tf/api_edurents/ApiEdurentsApplication.java
@@ -0,0 +1,13 @@
+package com.ingsoft.tf.api_edurents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEdurentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEdurentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsoft/tf/api_edurents/ApiEdurentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsoft.tf.api_edurents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEdurentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
