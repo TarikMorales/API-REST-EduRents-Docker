7a2ba1fb20833259e01f38a9a1e75fe60d5d5790
EduRS14 <eduardo14.05rivas@gmail.com>
2025-05-13 12:43:27 -0500

    refactor: Cambio del tipo de respuesta dada por las funciones en el controlador de producto por ResponseEntity

--- src/main/java/com/ingsoft/tf/api_edurents/controller/AdminProductController.java
+++ src/main/java/com/ingsoft/tf/api_edurents/controller/AdminProductController.java
@@ -4,8 +4,10 @@ import com.ingsoft.tf.api_edurents.dto.product.ProductDTO;
 import com.ingsoft.tf.api_edurents.dto.product.ShowProductDTO;
 import com.ingsoft.tf.api_edurents.model.entity.product.Product;
 import com.ingsoft.tf.api_edurents.service.AdminProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,24 +20,28 @@ public class AdminProductController {
     private final AdminProductService adminProductService;
 
     @GetMapping
-    public List<ShowProductDTO> obtenerProductos(){
-        return adminProductService.obtenerTodosLosProductos();
+    public ResponseEntity<List<ShowProductDTO>> obtenerProductos(){
+        List<ShowProductDTO> productos = adminProductService.obtenerTodosLosProductos();
+        return new ResponseEntity<List<ShowProductDTO>>(productos, HttpStatus.OK);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ShowProductDTO crearProducto(@RequestBody ProductDTO productoDTO){
-        return adminProductService.crearProducto(productoDTO);
+    public ResponseEntity<ShowProductDTO> crearProducto(@Valid @RequestBody ProductDTO productoDTO){
+        ShowProductDTO producto = adminProductService.crearProducto(productoDTO);
+        return new ResponseEntity<ShowProductDTO>(producto, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ShowProductDTO editarProducto(@PathVariable Integer id, @RequestBody ProductDTO productoDTO){
-        return adminProductService.editarProducto(id, productoDTO);
+    public ResponseEntity<ShowProductDTO> editarProducto(@PathVariable Integer id, @Valid @RequestBody ProductDTO productoDTO){
+        ShowProductDTO producto = adminProductService.editarProducto(id, productoDTO);
+        return new ResponseEntity<ShowProductDTO>(producto, HttpStatus.OK);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void eliminarProducto(@PathVariable Integer id){
+    public ResponseEntity<ShowProductDTO> eliminarProducto(@PathVariable Integer id){
         adminProductService.eliminarProducto(id);
+        return new ResponseEntity<ShowProductDTO>(HttpStatus.NO_CONTENT);
     }
 }
