82b43cfaabf11177262618937ae6981c2e37b43e
TarikMorales <145626466+TarikMorales@users.noreply.github.com>
2025-05-13 10:47:01 -0500

    feat: Implementación excepciones y corrección de validaciones para cancelar una transaccion
    Se agregó una carpeta de excepciones y se mejoró la validación en el DTO de la transacción para que se valide correctamente todos los atributos que si o si se deben llenar, pero también las excepciones ayudaron para una mejor visualización del error cometido al buscar una transacción por su ID para cancelarla y eliminar

--- src/main/java/com/ingsoft/tf/api_edurents/dto/transfers/TransactionDTO.java
+++ src/main/java/com/ingsoft/tf/api_edurents/dto/transfers/TransactionDTO.java
@@ -5,17 +5,19 @@ import com.ingsoft.tf.api_edurents.model.entity.transfers.TransactionStatus;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TransactionDTO {
 
-    @NotBlank(message = "Siempre debe haber un producto")
+    @NotNull(message = "Siempre debe haber un producto")
     private Integer id_producto;
-    @NotBlank(message = "Siempre debe haber un usuario")
+    @NotNull(message = "Siempre debe haber un usuario")
     private Integer id_usuario;
 
-    @NotBlank(message = "Debes elegir un metodo de pago")
+    @NotNull(message = "Debes elegir un metodo de pago")
     @Enumerated(EnumType.STRING)
     private PaymentMethod metodo_pago;
+    
 }
