bb0df99e937e882496c97c8c6a5345e019f93a1c
leo-dev11 <U202213866@upc.edu.pe>
2025-05-14 13:10:07 -0500

    feature: Agregado el manejo de excepciones globales y la excepcioón en el ProductServiceImpl

--- src/main/java/com/ingsoft/tf/api_edurents/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ingsoft/tf/api_edurents/service/impl/ProductServiceImpl.java
@@ -3,26 +3,40 @@ package com.ingsoft.tf.api_edurents.service.impl;
 import com.ingsoft.tf.api_edurents.dto.ProductDTO;
 import com.ingsoft.tf.api_edurents.mapper.ProductMapper;
 import com.ingsoft.tf.api_edurents.model.entity.product.CategoriesProducts;
+import com.ingsoft.tf.api_edurents.model.entity.product.Category;
+import com.ingsoft.tf.api_edurents.model.entity.product.Product;
 import com.ingsoft.tf.api_edurents.repository.product.CategoriesProductsRepository;
+import com.ingsoft.tf.api_edurents.repository.product.CategoryRepository;
 import com.ingsoft.tf.api_edurents.repository.product.ProductRepository;
 import com.ingsoft.tf.api_edurents.service.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
+    public ProductServiceImpl(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
     public List<ProductDTO> findByCategoryId(Integer categoryId) {
-        return ProductMapper.toDTOs(productRepository.findByCategoryId(categoryId));
+        categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category ID not found: " + categoryId));
+
+        List<Product> products = productRepository.findByCategoryId(categoryId);
+
+        if (products.isEmpty()) {
+            throw new RuntimeException("No products found for category ID: " + categoryId);
+        }
+        return ProductMapper.toDTOs(products);
     }
 
     @Override
