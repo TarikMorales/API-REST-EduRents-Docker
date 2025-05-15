12d7d9c28c56c465f359fe5806b3d988b784901f
EduRS14 <eduardo14.05rivas@gmail.com>
2025-05-15 00:06:01 -0500

    refactor: Cambio de nombre de archivo de controlador de la categoría y del tipo de respuesta de obtener categorías a ResponseEntity

--- src/main/java/com/ingsoft/tf/api_edurents/controller/AdminCategoryController.java
+++ src/main/java/com/ingsoft/tf/api_edurents/controller/AdminCategoryController.java
@@ -3,6 +3,8 @@ package com.ingsoft.tf.api_edurents.controller;
 import com.ingsoft.tf.api_edurents.dto.CategoryDTO;
 import com.ingsoft.tf.api_edurents.service.product.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/category")
-public class CategoryController {
+public class AdminCategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<CategoryDTO> getAllCategories(){
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<CategoryDTO>> getAllCategories(){
+        List<CategoryDTO> categoryDTOList = categoryService.getAllCategories();
+        return new ResponseEntity<>(categoryDTOList, HttpStatus.OK);
     }
 }
--- src/main/java/com/ingsoft/tf/api_edurents/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ingsoft/tf/api_edurents/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,14 @@
 package com.ingsoft.tf.api_edurents.service.impl;
 
 import com.ingsoft.tf.api_edurents.dto.CategoryDTO;
+import com.ingsoft.tf.api_edurents.exception.ResourceNotFoundException;
 import com.ingsoft.tf.api_edurents.mapper.CategoryMapper;
 import com.ingsoft.tf.api_edurents.model.entity.product.Category;
 import com.ingsoft.tf.api_edurents.repository.product.CategoryRepository;
 import com.ingsoft.tf.api_edurents.service.product.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,11 +21,12 @@ public class CategoryServiceImpl implements CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<CategoryDTO> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
         if (categories.isEmpty()) {
-            throw new RuntimeException("No categories found");
+            throw new ResourceNotFoundException("No se han encontrado categorías disponibles");
         }
         return CategoryMapper.toDTOs(categories);
     };
